f66638e7715b0fa68309fc8bac5b5581750e1357
hellenw <36727767+hellenw@users.noreply.github.com>
2018-05-05 10:53:06 +0800

    Add files via upload

---
+++ minesweeper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 30 17:09:38 2018
+
+@author: hellenwang
+"""
+
+class Minesweeper:
+    def __init__(self, player1, player2):
+        self.size = 9
+        self.player1 = player1
+        self.player2 = player2
+        
+    def board(self):
+        frame = {}
+        letters = "ABCDEFGHI"
+        for i in letters:
+            for i in range(1, self.size + 1):
+                
+        
+        
+        
+    def generate_mines(self, mines_num):
+        
+        
+        
